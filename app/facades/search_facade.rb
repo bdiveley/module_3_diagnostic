91dab27c9e7fbe7f6ba6149860d51344bd9ee219
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-12 09:46:51 -0700

    Working through stations method in search_facade which will return json

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -3,4 +3,16 @@ class SearchFacade
   def initialize(data)
     @data = data
   end
+
+  def stations
+    #this should return an array of station objects
+    conn = Faraday.new(:url => 'https://developer.nrel.gov') do |faraday|
+      faraday.headers['X-Api-Key'] = ENV['nrel_key']
+      faraday.adapter Faraday.default_adapter
+    end
+    response = conn.get('/api/alt-fuel-stations/v1.json?zip_code=80205&limit=10')
+    data = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
+
 end
