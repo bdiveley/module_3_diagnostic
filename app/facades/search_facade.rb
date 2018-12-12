bff3bbd7b62026db870f34146f4bd72f860f3086
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-12 10:16:21 -0700

    Created station objects and go them to display on screen. now working on improving my test and adding webmock

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -5,14 +5,16 @@ class SearchFacade
   end
 
   def stations
-    #this should return an array of station objects
     conn = Faraday.new(:url => 'https://developer.nrel.gov') do |faraday|
       faraday.headers['X-Api-Key'] = ENV['nrel_key']
       faraday.adapter Faraday.default_adapter
     end
     response = conn.get("/api/alt-fuel-stations/v1/nearest.json?radius=6&fuel_type=ELEC,LPG&location=#{@data}&limit=10")
     data = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+
+    data[:fuel_stations].map do |station|
+      Station.new(station)
+    end
   end
 
 end
--- spec/features/user_can_access_stations_by_zip_spec.rb
+++ spec/features/user_can_access_stations_by_zip_spec.rb
@@ -2,24 +2,23 @@ require "rails_helper"
 
 feature"user can search by zip code" do
   scenario "#80203 and get 10 sorted electric/propane stations within 6 miles" do
-    # As a user
-    # When I visit "/"
     visit root_path
-    # And I fill in the search form with 80203 (Note: Use the existing search form)
+
     fill_in "q", with: "80203"
-    # And I click "Locate"
+
     click_button "Locate"
-    # Then I should be on page "/search"
+
     expect(current_path).to eq(search_path)
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     expect(page).to have_css(".station", count:10)
 
-    within(".station") do
-      #adjust this test later
-      expect(page).not_to have_content("Distance: 10")
+    within(first(".station")) do
+      expect(page).to be <= 10
+
     # And the stations should be limited to Electric and Propane
-      expect(page).to have_content("Fuel Types: Electric")
-      expect(page).to have_content("Fuel Types: Propane")
+      expect(page).to have_content("Fuel Types: ELEC")
+      expect(page).to have_content("Fuel Types: LPG")
+
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
       expect(page).to have_content("Name:")
       expect(page).to have_content("Address:")
