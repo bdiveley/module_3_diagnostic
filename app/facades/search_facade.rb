f8a4464e411d11586a74452821f34d2dd41de79f
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-12 10:00:18 -0700

    Played around with nrel api docs to figure out what parameters to send into url

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -10,7 +10,7 @@ class SearchFacade
       faraday.headers['X-Api-Key'] = ENV['nrel_key']
       faraday.adapter Faraday.default_adapter
     end
-    response = conn.get('/api/alt-fuel-stations/v1.json?zip_code=80205&limit=10')
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?radius=6&fuel_type=ELEC,LPG&location=#{@data}&limit=10")
     data = JSON.parse(response.body, symbolize_names: true)
     binding.pry
   end
--- spec/features/user_can_access_stations_by_zip_spec.rb
+++ spec/features/user_can_access_stations_by_zip_spec.rb
@@ -20,7 +20,6 @@ feature"user can search by zip code" do
     # And the stations should be limited to Electric and Propane
       expect(page).to have_content("Fuel Types: Electric")
       expect(page).to have_content("Fuel Types: Propane")
-      expect(page).not_to have_content("Fuel Types: Coal")
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
       expect(page).to have_content("Name:")
       expect(page).to have_content("Address:")
